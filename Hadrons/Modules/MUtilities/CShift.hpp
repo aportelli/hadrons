f55539448e9e04675c5afb12dcddea3f5bacc95a
Ryan Hill <rchrys.hill@gmail.com>
2025-01-21 13:06:18 +0000

    Update CShift to support env positions

--- Hadrons/Modules/MUtilities/CShift.hpp
+++ Hadrons/Modules/MUtilities/CShift.hpp
@@ -1,41 +1,15 @@
-/*
- * CShift.hpp, part of Hadrons (https://github.com/aportelli/Hadrons)
- *
- * Copyright (C) 2015 - 2022
- *
- * Author: Antonin Portelli <antonin.portelli@me.com>
- * Author: Ryan Hill <rchrys.hill@gmail.com>
- * Author: Matthew Black <matthewkblack@protonmail.com>
- *
- * Hadrons is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Hadrons is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Hadrons.  If not, see <http://www.gnu.org/licenses/>.
- *
- * See the full license in the file "LICENSE" in the top level distribution 
- * directory.
- */
-
-/*  END LEGAL */
 #ifndef Hadrons_MUtilities_CShift_hpp_
 #define Hadrons_MUtilities_CShift_hpp_
 
 #include <Hadrons/Global.hpp>
 #include <Hadrons/Module.hpp>
 #include <Hadrons/ModuleFactory.hpp>
+#include <Hadrons/Serialization.hpp>
 
 BEGIN_HADRONS_NAMESPACE
 
 /******************************************************************************
- *                            Field CShift                                  *
+ *                         CShift                                 *
  ******************************************************************************/
 BEGIN_MODULE_NAMESPACE(MUtilities)
 
@@ -43,7 +17,7 @@ class CShiftPar: Serializable
 {
 public:
     GRID_SERIALIZABLE_CLASS_MEMBERS(CShiftPar,
-                                    std::string, field,
+                                    std::string, q,
                                     std::string, shift);
 };
 
@@ -62,13 +36,16 @@ public:
     virtual void setup(void);
     // execution
     virtual void execute(void);
+
+    template<typename T>
+    std::vector<T> getEnvVector(const std::string& value);
 };
 
-MODULE_REGISTER_TMP(CShiftGaugeField, TCShift<GIMPL::GaugeField>, MUtilities);
+MODULE_REGISTER_TMP(CShiftGaugeField,      TCShift<GIMPL::GaugeField>,      MUtilities);
 MODULE_REGISTER_TMP(CShiftPropagatorField, TCShift<FIMPL::PropagatorField>, MUtilities);
 
 /******************************************************************************
- *                     TCShift implementation                          *
+ *                 TCShift implementation                             *
  ******************************************************************************/
 // constructor /////////////////////////////////////////////////////////////////
 template <typename Field>
@@ -80,7 +57,12 @@ TCShift<Field>::TCShift(const std::string name)
 template <typename Field>
 std::vector<std::string> TCShift<Field>::getInput(void)
 {
-    std::vector<std::string> in = {par().field};
+    std::vector<std::string> in = {par().q};
+
+    if ((!isVector<int>(par().shift)) && (!par().shift.empty()))
+    {
+        in.push_back(par().shift);
+    }
     
     return in;
 }
@@ -97,25 +79,52 @@ std::vector<std::string> TCShift<Field>::getOutput(void)
 template <typename Field>
 void TCShift<Field>::setup(void)
 {
-    envCreateLat(Field, getName());
+    envCreate(Field, getName(), 1, envGetGrid(Field));
+}
+
+template <typename Field>
+template<typename T>
+std::vector<T> TCShift<Field>::getEnvVector(const std::string& value)
+{
+    std::vector<T> res = strToVec<T>(value);
+    if (res.size() <= 1)
+    {
+        if (envHasType(std::vector<T>, value))
+        {
+            LOG(Message) << "Trying to retrieve std::vector<T>" << std::endl;
+            return envGet(std::vector<T>, value);
+        }
+        else if (envHasType(HadronsSerializable, value))
+        {
+            LOG(Message) << "Trying to retrieve HadronsSerializable" << std::endl;
+            auto &t = envGet(HadronsSerializable, value);
+            return t.template get<std::vector<T>>();
+        }
+        else
+        {
+            HADRONS_ERROR(Definition, "cannot interpret '" + value + "' as either a literal or environment-stored vector");
+        }
+    }
+    LOG(Message) << "Trying to retrieve strToVec<T>" << std::endl;
+    return res;
 }
 
 // execution ///////////////////////////////////////////////////////////////////
 template <typename Field>
 void TCShift<Field>::execute(void)
 {
-    Coordinate xvec = strToVec<int>(par().shift);
-    LOG(Message) << "Shifting '" << par().field << "' by vector [" 
-                 << xvec[0] << " " << xvec[1] << " "
-                 << xvec[2] << " " << xvec[3] << "]." << std::endl;
+    // Set up shift variables
+    Coordinate             coord = getEnvVector<int>(par().shift);
+    const Field& prop  = envGet(Field, par().q);
+    Field&       out   = envGet(Field, getName());
 
-    auto &U   = envGet(Field, par().field);
-    auto &Utr = envGet(Field, getName());
-    Utr = U;
-    
-    for (int dir = 0; dir < Nd; dir++) {
-        int coord = xvec[dir];
-        if (coord != 0) Utr = Cshift(Utr,dir,coord);
+    // Execute CShift
+    out = prop;
+    for (int mu=0;mu<coord.size();mu++) 
+    {
+        int shift = coord[mu];
+        if (shift != 0)
+            out = Cshift(out,mu,shift);
     }
 }
 
