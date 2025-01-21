5e1a100ce7d74829c39074a076fb0f227ee17868
Ryan Hill <rchrys.hill@gmail.com>
2025-01-21 12:58:40 +0000

    Optimise and add trace calculation to DiscLoop
    Co-authored-by: Raoul Hodgson <raoul.hodgson@desy.de>

--- Hadrons/Modules/MContraction/TraceSum.cpp
+++
@@ -1,7 +0,0 @@
-#include <Hadrons/Modules/MContraction/TraceSum.hpp>
-
-using namespace Grid;
-using namespace Hadrons;
-using namespace MContraction;
-
-template class HADRONS_NAMESPACE::MContraction::TTraceSum<FIMPL>;
