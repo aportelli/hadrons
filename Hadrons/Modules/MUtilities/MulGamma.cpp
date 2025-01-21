e7bc3dbbcf6ff278560adf5f34d696a41c733498
Ryan Hill <rchrys.hill@gmail.com>
2025-01-21 13:00:07 +0000

    Remove outdated QED disconnected workflow modules

--- Hadrons/Modules/MUtilities/MulGamma.cpp
+++
@@ -1,7 +0,0 @@
-#include <Hadrons/Modules/MUtilities/MulGamma.hpp>
-
-using namespace Grid;
-using namespace Hadrons;
-using namespace MUtilities;
-
-template class HADRONS_NAMESPACE::MUtilities::TMulGamma<FIMPL>;
